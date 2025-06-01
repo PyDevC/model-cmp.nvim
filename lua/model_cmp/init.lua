5283b544d9c6fcee17f98fa717429bbefcebf4f6
PyDevC <anuskcse@gmail.com>
2025-06-01 15:16:34 +0530

    [Update]: New model, setup config

---
+++ lua/model_cmp/init.lua
@@ -0,0 +1,63 @@
+local model_cards_list = require("model_cmp.model_cards_list")
+local default_config = {} -- will add config later
+
+local M = {}
+
+function M.setup(config)
+  M.presets = config.presets or {}
+  M.presets.original = config
+
+  config.presets = nil
+  M.config = vim.tbl_deep_extend('force', default_config, config or {})
+end
+
+local function choose_model_card()
+  vim.ui.select(model_cards_list.model_cards_list, {
+    prompt = "Select the model card: ",
+    format_item = function(item)
+      return item
+    end,
+  }, function(choosen)
+    if choosen then
+      return M.change_model(choosen)
+    end
+  end)
+end
+
+function M.change_model(model_card)
+  model_card = model_card or choose_model_card()
+  vim.notify("Model Changed to: " .. model_card.name)
+end
+
+vim.api.nvim_create_user_command('model_cmp', function(args)
+  local fargs = args.fargs
+  local actions = {}
+
+  actions.ghosttext = {
+    enable = require('model_cmp.ghosttext').action.enable_auto_trigger,
+    disable = require('model_cmp.ghosttext').action.disable_auto_trigger,
+    toggle = require('model_cmp.ghosttext').action.toggle_auto_trigger,
+  }
+end, {
+  nargs = '+',
+  complete = function(_, cmdline, _)
+    cmdline = cmdline or ''
+
+    if cmdline:find 'change_model' then
+      return M.change_model()
+    end
+
+    if cmdline:find 'ghosttext' then
+      return {
+        'enable',
+        'disable',
+        'toggle',
+      }
+    end
+
+    return { 'ghosttext' }
+  end,
+})
+
+
+return M
--- modelapi/hf_model/model_cards/deepseek_coder.py
+++ modelapi/hf_model/model_cards/deepseek_coder.py
@@ -11,12 +11,11 @@ class DeepSeek_Coder_Base(BaseModelCard):
         print(dir(self.model))
         print(self.model.__class__.__name__)
 
-    def generate(self, message, max_new_tokens=900, batch_size=64):
+    def generate(self, message, max_new_tokens=9000, batch_size=64):
         inputs = self.tokenizer(message, return_tensors="pt").to(self.model.device)
         outputs = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
         decoded_output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)[len(message):]
         return decoded_output
-
 
 class DeepSeek_Coder_6_7B_Instruct(DeepSeek_Coder_Base):
     def __init__(self):
@@ -29,4 +28,16 @@ class DeepSeek_Coder_V2_Instruct(DeepSeek_Coder_Base):
     def __init__(self):
         model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Instruct", torch_dtype=torch.float16).cuda()
         tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Instruct")
+        super().__init__(model, tokenizer)
+
+class DeepSeek_Coder_V2_Instruct_Quant(DeepSeek_Coder_Base):
+    def __init__(self):
+        model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Instruct", torch_dtype=torch.float16, load_in_8bit=True).cuda()
+        tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Instruct")
+        super().__init__(model, tokenizer)
+
+class DeepSeek_Coder_6_7B_Instruct_Quant(DeepSeek_Coder_Base):
+    def __init__(self):
+        model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", torch_dtype=torch.float16, load_in_8bit=True).cuda()
+        tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct")
         super().__init__(model, tokenizer)
