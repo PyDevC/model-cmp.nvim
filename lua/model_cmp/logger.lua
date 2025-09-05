0e8cb22b708ff28cb2689e10c832cfaec0984288
PyDevC <anuskcse@gmail.com>
2025-09-05 06:40:48 -0400

    Logger: new logging utility for model_cmp
    We need to integrate this all over plugin
    Need to save logs only at the time of neovim exit
    Also need this for ModelCmpLogs

---
+++ lua/model_cmp/logger.lua
@@ -0,0 +1,80 @@
+local M = {}
+
+---@class Logs
+---@field timestamp string
+---@field loglist table<LogInstance>
+
+---@class LogInstance
+---@field type vim.log.levels
+---@field message string
+
+M.Logs = {}
+
+---@param log LogInstance
+local function add_log(log)
+    local strtype = string.format("[%s]", log.type)
+    local modifiedlog = strtype .. log.message
+    table.insert(M.Logs, modifiedlog)
+end
+
+---@return LogInstance
+local function log_template()
+    local log = {
+        type = vim.log.levels.OFF,
+        message = ""
+    }
+    return log
+end
+
+function M.save_logs(logdir)
+    logdir = logdir or vim.fn.stdpath('log')
+    local filepath = logdir .. "model_cmp.log"
+
+    local strlogs = ""
+
+    for k, v in ipairs(M.Logs) do
+        strlogs = strlogs .. v .. "\n"
+    end
+
+    vim.fn.writefile(strlogs, filepath, 'a')
+end
+
+function M.info(message)
+    local log = log_template()
+    log.type = vim.log.levels.INFO
+    log.message = message
+    add_log(log)
+    vim.notify(message, log.type)
+end
+
+function M.warning(message)
+    local log = log_template()
+    log.type = vim.log.levels.WARN
+    log.message = message
+    add_log(log)
+    vim.notify_once(message, log.type)
+end
+
+function M.trace(message)
+    local log = log_template()
+    log.type = vim.log.levels.TRACE
+    log.message = message
+    add_log(log)
+end
+
+function M.debugging(message)
+    local log = log_template()
+    log.type = vim.log.levels.DEBUG
+    log.message = message
+    add_log(log)
+end
+
+function M.error(message)
+    local log = log_template()
+    log.type = vim.log.levels.ERROR
+    log.message = message
+    add_log(log)
+    vim.notify(message, log.type)
+end
+
+return M
