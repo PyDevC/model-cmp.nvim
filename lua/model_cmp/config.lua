e977869898ff70cffade1e908069bbdac5eb0e73
PyDevC <anuskcse@gmail.com>
2025-09-17 07:57:22 -0400

    Cleanup and better code structure
    clean init file
    added more descriptive options for config

--- lua/model_cmp/config.lua
+++ lua/model_cmp/config.lua
@@ -1,21 +1,28 @@
-local apiconfig = require("model_cmp.modelapi.apiconfig")
-
-local M = {}
-
 ---@class ModelCmp.Config
+---@field integrations table<string, boolean>
+---@field request_delay integer
 ---@field api ModelCmp.Modelapi.Config
 ---@field virtualtext table<string, string> virtual text config
+
+local M = {}
 
 ---@return ModelCmp.Config
 function M.default()
     return {
-        delay = 1000,
-        api = apiconfig.default(),
-
+        -- These integrations are future integrations so setting these will not work
+        integrations = {
+            lspconfig = true,
+            cmp = true,
+            coq = true,
+            blink = true
+        },
+        request_delay = 1000,
+        api = require("model_cmp.modelapi.apiconfig").default(),
         virtualtext = {
             enable = false,
             type = "inline",
-            style = { -- This is just a highlight group
+            style = {
+                -- Setup the Highlight group for Virtual text suggestions
                 fg = "#b53a3a",
                 italic = false,
                 bold = false
@@ -24,4 +31,22 @@ function M.default()
     }
 end
 
-return M
+---@type ModelCmp.Config
+local options
+
+---@param opts? ModelCmp.Config
+---@return ModelCmp.Config
+function M.setup(opts)
+    opts = opts or {}
+    ---@type ModelCmp.Config
+    options = vim.tbl_deep_extend("force", M.default(), opts)
+    require("model_cmp.commands").setup()
+    return options
+end
+
+return setmetatable(M, {
+  __index = function(_, key)
+    options = options or M.setup()
+    return options[key]
+  end,
+})
